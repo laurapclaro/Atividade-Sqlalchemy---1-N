e8a750ae5d36953835b0e09064fe725d3ecf49db
Laura <laurapapa693@gmail.com>
2026-04-06 11:03:09 -0300

    Correção do código

--- main.py
+++ main.py
@@ -5,17 +5,15 @@ engine = create_engine("sqlite:///estoque_farmacia.db", echo=False)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
-
-
 class Farmacia(Base):
     __tablename__ = "farmacia"
 
     id   = Column(Integer, primary_key=True)
     nome = Column(String(100), nullable=False)
-    endereço  = Column(String(14), unique=True)
+    endereco  = Column(String(140), unique=True)
 
     # Acesso aos atendimentos deste paciente
-    medicamentos = relationship("Medicamento", back_populates="farmacia", cascade="all, delete-orphan")
+    medicamentos = relationship("Medicamento", back_populates="farmacia")
 
     def __repr__(self):
         return f"<Farmacia(id={self.id}, nome='{self.nome}')>"
@@ -25,8 +23,12 @@ class Medicamento(Base):
 
     id = Column(Integer, primary_key=True)
     nome = Column(String(100), nullable=False)
-    preço = Column(Integer, primary_key=True)
+    preco = Column(Integer, primary_key=True)
     estoque = Column(Integer, primary_key=True)
+
+    farmacia_id = Column(Integer, ForeignKey("farmacia.id"), nullable=False)
+    farmacia = relationship("Farmacia", back_populates="medicamentos")
+
 
     def __repr__(self):
         return f"<Medicamento(nome='{self.nome}', estoque={self.estoque})>"
@@ -35,6 +37,46 @@ class Medicamento(Base):
 #Criar banco de dados
 
 Base.metadata.create_all(engine)
+
+#FUNÇÕES
+
+def cadastrar_farmacia():
+    with Session() as session:
+        try:
+            nome_farmacia = input("Digite o nome da farmacia: ").capitalize()
+            endereco_input = input("Digite o endereço da farmacia:")
+            farmacia = Farmacia(nome=nome_farmacia, endereco=endereco_input)
+            
+            session.add(farmacia)
+            session.commit()
+            print(f"Farmacia cadastrada com sucesso!")
+        except Exception as erro:
+            session.rollback()
+            print(f"Ocorreu um erro: {erro}")
+
+cadastrar_farmacia()
+
+
+def cadastrar_medicamentos():
+    with Session() as session:
+        try:
+            nome_medicamento = input("Digite o nome do medicamento: ").capitalize()
+            medicamento = session.query(Medicamento).filter_by(nome=nome_medicamento).first()
+            if medicamento == None:
+                print(f"Nenhum medicamento encontrado com esse nome: {nome_medicamento}")
+                return
+            else: 
+                preço = input("Digite o preço do medicamento: ").capitalize()
+                estoque = estoque(nome=preço)
+                session.add(medicamento)
+                session.commit()
+                print(f"Medicamento cadastrado com sucesso!")
+
+        except Exception as erro:
+            session.rollback()
+            print(f"Ocorreu um erro {erro} ")
+cadastrar_medicamentos()
+           
 
 
 
